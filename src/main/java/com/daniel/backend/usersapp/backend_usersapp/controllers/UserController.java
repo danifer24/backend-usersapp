f75a82c96181a450ca1eafe07173bafa10dbacf4
danifer24 <danielvf24@gmail.com>
2025-10-29 18:29:16 +0100

    Implementar metodos handler POST, PUT y DELETE del RestController

--- src/main/java/com/daniel/backend/usersapp/backend_usersapp/controllers/UserController.java
+++ src/main/java/com/daniel/backend/usersapp/backend_usersapp/controllers/UserController.java
@@ -4,9 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +38,32 @@ public class UserController {
             return ResponseEntity.ok(userOptional.orElseThrow());
         }
         return ResponseEntity.notFound().build();
-          
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createUser(@RequestBody User user){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(user));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@RequestBody User user, @PathVariable Long id){
+        Optional<User> o =service.findById(id);
+        if(o.isPresent()){
+            User userDb = o.orElseThrow();
+            userDb.setUsername(user.getUsername());
+            userDb.setEmail(user.getEmail());
+            return ResponseEntity.status(HttpStatus.CREATED).body(service.save(userDb));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> remove(@PathVariable Long id){
+        Optional<User> o = service.findById(id);
+        if(o.isPresent()){
+            service.remove(id);
+            return ResponseEntity.noContent().build(); // 204
+        }
+        return ResponseEntity.notFound().build();
     }
 }
