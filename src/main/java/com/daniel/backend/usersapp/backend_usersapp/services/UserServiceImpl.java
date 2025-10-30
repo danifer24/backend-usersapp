f70060beaa5855d5b6ab949cb3629c594d96c6d3
danifer24 <danielvf24@gmail.com>
2025-10-30 22:47:41 +0100

    Encriptar contraseña usuarios bbdd

--- src/main/java/com/daniel/backend/usersapp/backend_usersapp/services/UserServiceImpl.java
+++ src/main/java/com/daniel/backend/usersapp/backend_usersapp/services/UserServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +17,9 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository repository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     @Transactional(readOnly = true)
@@ -32,6 +36,7 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public User save(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return repository.save(user);
     }
 
