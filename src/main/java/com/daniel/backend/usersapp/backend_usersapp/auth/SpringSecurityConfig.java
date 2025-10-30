099bc6c80fc6702d139ab754e61779b57a4ec794
danifer24 <danielvf24@gmail.com>
2025-10-30 20:44:01 +0100

    Añadir filtro JwtAuthentication

--- src/main/java/com/daniel/backend/usersapp/backend_usersapp/auth/SpringSecurityConfig.java
+++ src/main/java/com/daniel/backend/usersapp/backend_usersapp/auth/SpringSecurityConfig.java
@@ -1,14 +1,21 @@
 package com.daniel.backend.usersapp.backend_usersapp.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.daniel.backend.usersapp.backend_usersapp.auth.filters.JwtAuthenticationFilter;
+
 @Configuration
 public class SpringSecurityConfig {
+
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -16,6 +23,7 @@ public class SpringSecurityConfig {
         .requestMatchers(HttpMethod.GET, "/users").permitAll()
         .anyRequest().authenticated()
         .and()
+        .addFilter(new JwtAuthenticationFilter(authenticationConfiguration.getAuthenticationManager()))
         .csrf(config -> config.disable())
         .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .build();
