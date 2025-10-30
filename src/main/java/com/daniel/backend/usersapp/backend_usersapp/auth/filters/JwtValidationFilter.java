1aa1610febec3005f6c45260cd09cb682d685c61
danifer24 <danielvf24@gmail.com>
2025-10-30 22:03:00 +0100

    Validar token

--- src/main/java/com/daniel/backend/usersapp/backend_usersapp/auth/filters/JwtValidationFilter.java
+++ src/main/java/com/daniel/backend/usersapp/backend_usersapp/auth/filters/JwtValidationFilter.java
@@ -1,9 +1,20 @@
 package com.daniel.backend.usersapp.backend_usersapp.auth.filters;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -21,13 +32,37 @@ public class JwtValidationFilter extends BasicAuthenticationFilter {
             throws IOException, ServletException {
 
         String header = request.getHeader("Authorization");
-        
-        if(header == null || !header.startsWith("Bearer ")){
+
+        if (header == null || !header.startsWith("Bearer ")) {
             chain.doFilter(request, response);
             return;
         }
 
         String token = header.replace("Bearer ", "");
+        byte[] tokenDecodeBytes = Base64.getDecoder().decode(token);
+        String tokenDecode = new String(tokenDecodeBytes);
+
+        String[] tokenArray = tokenDecode.split(".");
+        String secret = tokenArray[0];
+        String username = tokenArray[1];
+
+        if ("algun_token_con_alguna_frase_o_palabra_secreta".equals(secret)) {
+
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(username,
+                    authorities);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            chain.doFilter(request,response);
+        } else {
+            Map<String, String> body = new HashMap<>();
+            body.put("message", "El token no es valido");
+            
+            response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+            response.setStatus(403);
+            response.setContentType("application/json");
+        }
     }
 
 }
