b54b210a5eaa88d952f0bc8352f58126d3176b72
danifer24 <danielvf24@gmail.com>
2025-10-31 02:20:51 +0100

    Agregar clase EntityRole

--- src/main/java/com/daniel/backend/usersapp/backend_usersapp/models/entities/User.java
+++ src/main/java/com/daniel/backend/usersapp/backend_usersapp/models/entities/User.java
@@ -1,11 +1,14 @@
 package com.daniel.backend.usersapp.backend_usersapp.models.entities;
 
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -31,6 +34,9 @@ public class User {
     @Email
     @Column(unique = true)
     private String email;
+
+    @ManyToMany
+    private List<Role> roles;
 
     public Long getId() {
         return id;
@@ -62,6 +68,14 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
     }
 
 }
